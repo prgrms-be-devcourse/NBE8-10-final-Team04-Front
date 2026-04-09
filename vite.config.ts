5e8052c1dcee8aa89f0835fa4a1c9d5f899966dd
Minhee Han <yyj9694651@gmail.com>
2026-04-09 20:55:36 +0900

    [Chore/TM-221] 기본 설정 및 패키지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
+  plugins: [react(), babel({presets: [reactCompilerPreset()]}), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    port: 5173, // 포트 고정
+    strictPort: true, // 5173이 사용 중이면 서버를 띄우지 않음 (실수로 포트 바뀌는 것 방지)
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "unsafe-none", // 추가로 시도해 볼 만한 옵션
+    },
+  },
+});
